fb67c9711c0d945d4547ac7e9e2cbef4d4f28d7a
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-29 17:54:44 +0100

    Ex.4 - Declarei e instanciei 2 cronómetro + iniciei a contagem do 1º e pausei durante 0.5 segundos + iniciei o 2º e pausei durante 0.3 segundos + e finalmente parei os dois cronómetros e mostrei o tempo percorrido de ambos na consola em segundos.

---
+++ Tempo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(300);
+            crono1.Stop();
+            crono2.Stop();
+            double number1 = crono1.Elapsed.TotalSeconds;
+            double number2 = crono2.Elapsed.TotalSeconds;
+            Console.WriteLine("O Tempo decorrido do cronometro 1 é de " + number1.ToString() + " segundos.");
+            Console.WriteLine("O Tempo decorrido do cronometro 2 é de " + number2.ToString() + " segundos.");
+        }
+    }
+}
