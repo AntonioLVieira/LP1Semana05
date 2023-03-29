cae7e93b2567b824fb5a101aae2a1f367a2ccad5
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-29 18:20:24 +0100

    Ex.4 - pus a imprimir simplesmente o resultado a dar duas casas décimais + Inseri vários comentários

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -8,18 +8,33 @@ namespace Tempo
     {
         static void Main(string[] args)
         {
+            // Declarar e instanciar dois cronómetros diferentes
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
+
+            // O 1º cronómetro começa a contar o tempo
             crono1.Start();
+
+            // O 1º cronómetro pausa durante meio segundo
             Thread.Sleep(500);
+
+            // O 2º cronómetro começa a contar o tempo
             crono2.Start();
+
+            // O 2º cronómetro pausa durante o.3 segundos
             Thread.Sleep(300);
+
+            // Ambos os crónometros param de contar o tempo
             crono1.Stop();
             crono2.Stop();
+
+            // Converte os números double para segundos e dá os números da referência 
             double number1 = crono1.Elapsed.TotalSeconds;
             double number2 = crono2.Elapsed.TotalSeconds;
-            Console.WriteLine("O Tempo decorrido do cronometro 1 é de " + number1.ToString() + " segundos.");
-            Console.WriteLine("O Tempo decorrido do cronometro 2 é de " + number2.ToString() + " segundos.");
+
+            // Converte os números double para string e apresenta na consola o tempo de cada cronómetro com 2 casas décimais
+            Console.WriteLine("O Tempo decorrido do cronometro 1 é de " + number1.ToString("F2") + " segundos.");
+            Console.WriteLine("O Tempo decorrido do cronometro 2 é de " + number2.ToString("F2") + " segundos.");
         }
     }
 }
