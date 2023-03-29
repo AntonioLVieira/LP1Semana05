29ddd79bd0f6037ec03f0eb2f951212e7530d444
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-29 19:13:33 +0100

    Ex.7 - Peço o número de dados ao utilizador + Uso o Random para cada dado (vai me dar um valor aleatório de cada dado) + Soma de todos os resultados e mostra na consola

---
+++ RandomDice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me the number of dices: ");
+            int dices = int.Parse(Console.ReadLine());
+            Random number = new Random();
+            int sum = 0;
+            for (int i = 0; i < dices; i++)
+            {
+                int dice = number.Next(1,7);
+                sum = sum + dice;
+            }
+            Console.WriteLine("The sum of all the results from the dices is: " + sum);
+
+        }
+    }
+}
