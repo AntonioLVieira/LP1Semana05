7c0d7aa4be24a802b92e45b60e27efde6f3ea3b0
AntonioLVieira <antonio.allvieira@gmail.com>
2024-03-21 11:46:57 +0000

    resoluçao do exercicio 1 (Stopwatch)

---
+++ Chronos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cronometro1 = new System.Diagnostics.Stopwatch();
+            var cronometro2 = new System.Diagnostics.Stopwatch();
+
+            cronometro1.Start();
+
+            Thread.Sleep(600);
+
+            cronometro2.Start();
+
+            Thread.Sleep(200);
+
+            cronometro1.Stop();
+            cronometro2.Stop();
+
+            Console.WriteLine("Tempo decorrido em crononometro 1: {0:F3} segundos", crono1.Elapsed.TotalSeconds);
+            Console.WriteLine("Tempo decorrido em cronometro 2: {0:F3} segundos", crono2.Elapsed.TotalSeconds);
+        }
+    }
+}
