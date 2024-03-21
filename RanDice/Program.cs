b7c346b6e3455c13c8f212bdc2a3d9ab7af9887a
AntonioLVieira <antonio.allvieira@gmail.com>
2024-03-21 12:07:39 +0000

    resoluçao do exercicio 5 (Dados)

---
+++ RanDice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Uso: dotnet run --project RanDice -- <número_de_dados> <semente>");
+                return;
+            }
+
+            if (!int.TryParse(args[0], out int numberOfDice) || !int.TryParse(args[1], out int seed))
+            {
+                Console.WriteLine("Os argumentos não são válidos.");
+                return;
+            }
+
+            Random random = new Random(seed);
+            int sum = 0;
+
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                int diceValue = random.Next(1, 7); 
+                sum += diceValue;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
