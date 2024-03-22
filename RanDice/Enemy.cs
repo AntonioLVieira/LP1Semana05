55ad121f34ea800c8d08ae63f00f837566d0dd59
AntonioLVieira <antonio.allvieira@gmail.com>
2024-03-22 16:54:29 +0000

    Resolução exercicio 7 (adicionar construtor)

--- RanDice/Enemy.cs
+++ RanDice/Enemy.cs
@@ -2,6 +2,11 @@ namespace MyGame
 {
     public class Enemy
     {
-        
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
