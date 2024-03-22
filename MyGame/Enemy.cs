ed7b5498b8124d4403293c634c1d628cba817072
AntonioLVieira <antonio.allvieira@gmail.com>
2024-03-22 17:08:45 +0000

    Changed the file enemy.cs from ranDice to MyGame + correction of some build errors

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -2,24 +2,30 @@ namespace MyGame
 {
     public class Enemy
     {
+        private string name; 
+        private float health;
+        private float shield;
+
         public Enemy(string name)
         {
             this.name = name;
             health = 100;
             shield = 0;
         }
+
         public string GetName()
         {
             return name;
         }
+
         public void TakeDamage(float damage)
         {
             shield -= damage;
             if (shield < 0)
             {
-                float damageStillTolnflict =- shield;
+                float damageStillToInflict = -shield;
                 shield = 0;
-                health -= damageStillTolnflict;
+                health -= damageStillToInflict;
                 if (health < 0) health = 0;
             }
         }
